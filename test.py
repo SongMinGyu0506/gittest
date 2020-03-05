3c8d674a7f2a8eb11c6574a01e2e27d4b65252bd
SongMinGyu <ggp04114@naver.com>
2020-03-05 21:03:07 +0900

    test_1

---
+++ test.py
@@ -0,0 +1,27 @@
+num_student = int(input())
+student_list = []
+
+for _ in range(num_student):
+    weight, height = map(int, input().split())
+    student_list.append((weight, height))
+
+for i in student_list:
+    rank = 1
+    for j in student_list:
+        if i[0] < j[0] and i[1] < j[1]:
+                rank += 1
+    print(rank, end = " ")
+
+'''input_d = int(input())
+student_list = []
+
+for i in range(student_list):
+    weight, height = map(int, input().split())
+    student_list.append(weight,height)
+
+for i in student_list:
+    rank = 1
+    for j in student_list:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank = rank+1
+        print(rank, end=' ')'''
